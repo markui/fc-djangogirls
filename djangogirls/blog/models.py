79eb55335c1cdcef2e9a198dfac577e0ec15e426
Mark Kim <viking617617@gmail.com>
2017-09-25 14:34:24 +0900

    Post model 정의, migrations생성, admin 추가

---
+++ djangogirls/blog/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+
+# Create your models here.
+
+class Post(models.Model):
+    # settings.AUTH_USER_MODEL > auth.User
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
