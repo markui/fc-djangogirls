67e705ed394c801ea8ea4193aecb4abb6714ef3c
Mark Kim <viking617617@gmail.com>
2017-09-25 16:25:29 +0900

    post_list view에서 template, context 변수 사용

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return HttpResponse('Post list')
+    posts = Post.objects.all()
+    context = {
+        # posts key의 value는 QuerySet
+        'posts': posts
+    }
+    return render(request, 'blog/post_list.html', context)
+    # 단순 html파일이 아니다. -> template언어를 장고 내부에서 사용해 html 파일을 렌더링해서 사용자 요청에 대해 보내주는 것
